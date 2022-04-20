66bb80e066d4369ff41271f239e9a7fe30c62e79
Santiago Emanuel Paz <santimanupaz@gmail.com>
2022-04-19 23:48:28 -0300

    Cambie el evento que tenia en el html y añadi el nombre del juego

--- Simulador de costos Steam/index.js
+++ Simulador de costos Steam/index.js
@@ -1,4 +1,5 @@
 // VARIABLES 
+let nombreJuego;
 let engañaPichanga = false;
 let precioJuego;
 let precioFinal;
@@ -6,11 +7,15 @@ let impuestoRetencionGanancias;
 let impuestoPais; 
 let busquedasRecientes = []; 
 
+
+
 // FUNCIONES
 
 
+
+
 function imprimirResumen(){
-    document.getElementById("precioTotalConImpuestos").innerHTML = ` <br/> El precio del juego es:  $  ${Math.floor (precioJuego)}  <br/><br/>  Retención de Ganancias - RG AFIP Nº 4815/2020  ( 35% )     $    ${Math.floor (impuestoRetencionGanancias)}  <br/><br/>  Impuesto PAIS - RG AFIP N° 4659/2020  ( 30% )     $     ${Math.floor (impuestoPais)}  <br/><br/> <strong> EL VALOR CON IMPUESTOS ES DE:     $   ${Math.floor (precioFinal)} <strong/> <br/><br/> `;
+    document.getElementById("precioTotalConImpuestos").innerHTML = ` <br/> El precio del juego es:  $  ${Math.floor (precioJuego)}  <br/><br/>  Retención de Ganancias - RG AFIP Nº 4815/2020  ( 35% )     $    ${Math.floor (impuestoRetencionGanancias)}  <br/><br/>  Impuesto PAIS - RG AFIP N° 4659/2020  ( 30% )     $     ${Math.floor (impuestoPais)}  <br/><br/> <strong class="precioFinalTexto"> EL VALOR CON IMPUESTOS ES DE:     $   ${Math.floor (precioFinal)} </strong> <br/><br/> `;
 }
 
 
@@ -19,17 +24,19 @@ function calcularImpuestos(){
     if (engañaPichanga === true) {
 
         if (document.getElementById("precioJuego").value !== "") {
-            precioJuego = parseInt(document.getElementById("precioJuego").value);
+        nombreJuego = document.querySelector("#nombreJuego").value;
+        precioJuego = parseInt(document.getElementById("precioJuego").value);
         impuestoRetencionGanancias = precioJuego * 0.35;
         impuestoPais = precioJuego * 0.3;
         precioFinal = precioJuego + impuestoRetencionGanancias + impuestoPais;
+        
     
         if (precioJuego < 10) {
             alert("No es un precio valido");
             document.getElementById("precioJuego").focus();   
         } else{
             imprimirResumen();
-            busquedasRecientes.push(`Precio original:  AR$ ${Math.floor (precioJuego)} -  Con impuestos: AR$ ${Math.floor (precioFinal)} <br/>`);
+            busquedasRecientes.push(`<strong>${(nombreJuego)}</strong><br> Precio original:  <strong> AR$ ${Math.floor (precioJuego)} </strong>  -    Con impuestos: <strong> AR$ ${Math.floor (precioFinal)} </strong> <br>`);
     
         }
         
@@ -52,8 +59,13 @@ function calcularImpuestos(){
 
 
 // Programa 
+document.querySelector("#btnCalculo").onclick = calcularImpuestos;
 
-calcularImpuestos();
+
+
+
+   
+
 
 
 
